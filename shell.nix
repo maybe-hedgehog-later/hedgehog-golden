fcba974e0feff6e98b42677c7c43bf4966595fb7
Felix Mulder <felix.mulder@gmail.com>
2019-07-21 19:43:00 +0200

    Remove unnecessary cruft from shell.nix

--- shell.nix
+++ shell.nix
@@ -5,14 +5,8 @@ with import (builtins.fetchGit {
   rev = "caacbe98ce1d9cb6c8aab9d5f210dab6bb689c8a";
 }) {};
 
-#with import (builtins.fetchGit {
-#  name = "nixos-release-19.03";
-#  url = https://github.com/nixos/nixpkgs/;
-#  ref = "release-19.03";
-#}) {};
-
 stdenv.mkDerivation {
-  name = "thut";
+  name = "hedgehog-golden";
 
   buildInputs = with haskell.packages.ghc864; [
     cabal-install
