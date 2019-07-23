3e9ed88882e934b0d17b3b56db12480163455253
Felix Mulder <felix.mulder@gmail.com>
2019-07-23 18:02:48 +0200

    Implement round checking for when file already exists

--- hedgehog-golden-aeson/src/Hedgehog/Golden/Aeson.hs
+++ hedgehog-golden-aeson/src/Hedgehog/Golden/Aeson.hs
@@ -7,12 +7,15 @@ import           Prelude hiding (writeFile)
 import           Control.Monad.Extra (ifM)
 import           Control.Monad.IO.Class (MonadIO(..))
 import qualified Data.Aeson as Aeson
-import           Data.Aeson (ToJSON, (.=))
+import qualified Data.Aeson.Types as Aeson
+import           Data.Aeson (FromJSON, ToJSON, (.=), (.:))
 import           Data.Aeson.Encode.Pretty (Config(..), Indent(..), encodePretty', defConfig)
 import           Data.ByteString.Lazy (ByteString, writeFile)
 import           Data.Proxy (Proxy(..))
+import           Data.Sequence (Seq)
 import           Data.Typeable (Typeable, typeRep)
-import           Hedgehog (Gen, Seed(..))
+import           GHC.Stack (HasCallStack, withFrozenCallStack)
+import           Hedgehog
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Internal.Gen as Gen
 import qualified Hedgehog.Internal.Seed as Seed
@@ -20,36 +23,48 @@ import qualified Hedgehog.Internal.Tree as Tree
 import qualified Hedgehog.Range as Range
 import           System.Directory (doesFileExist, createDirectoryIfMissing)
 
-goldenProperty :: forall a m
-   . Typeable a
-  => MonadIO m
+goldenProperty :: forall a
+   . HasCallStack
+  => Typeable a
+  => FromJSON a
   => ToJSON a
-  => Gen a -> m ()
+  => Eq a
+  => Show a
+  => Gen a -> Property
 goldenProperty gen =
   let
     typeName = show . typeRep $ Proxy @a
     fileName = "./golden/" <> typeName <> ".json"
     fileExists = liftIO . doesFileExist
-    compareExisting = undefined
   in
-    ifM
+    withFrozenCallStack . property $ ifM
       (fileExists fileName)
       (compareExisting fileName gen)
       (createNewFile fileName gen)
 
-createNewFile :: MonadIO m => ToJSON a => FilePath -> Gen a -> m ()
+createNewFile :: MonadTest m => MonadIO m => ToJSON a => FilePath -> Gen a -> m ()
 createNewFile fp gen = Seed.random >>= \seed ->
   let
-    Just samples =
-      Gen.evalGen 10 seed $
-        Aeson.toJSON <$> Gen.seq (Range.singleton 10) gen
-    object = Aeson.object
-      [ "seed" .= aesonSeed seed
-      , "samples" .= Tree.treeValue samples
-      ]
+    samples =
+      Aeson.toJSON <$> genSamples seed gen
+    object =
+      Aeson.object [ "seed" .= aesonSeed seed, "samples" .= samples ]
   in liftIO $ do
     createDirectoryIfMissing True "golden"
     writeFile fp (encodePretty object)
+
+genSamples :: Seed -> Gen a -> Seq a
+genSamples seed gen =
+  let
+    loop n = \case
+      Just tree -> Tree.treeValue tree
+      Nothing ->
+        if n < 0 then
+          error "Too many discards - abandoned generating samples"
+        else
+          loop (n - 1) . Gen.evalGen 0 seed $ Gen.seq (Range.singleton 10) gen
+  in
+    loop (100 :: Int) Nothing
 
 aesonSeed :: Seed -> Aeson.Value
 aesonSeed (Seed value gamma) =
@@ -60,3 +75,26 @@ encodePretty = encodePretty' defConfig
   { confIndent = Spaces 2
   , confCompare = compare
   }
+
+compareExisting :: MonadTest m => MonadIO m => Show a => Eq a => FromJSON a => FilePath -> Gen a -> m ()
+compareExisting fileName gen =
+  liftIO (Aeson.eitherDecodeFileStrict fileName) >>=
+  getSeedAndElements >>=
+  uncurry (compareWith gen)
+
+getSeedAndElements :: MonadTest m => FromJSON a => Either String Aeson.Object -> m (Seed, Seq a)
+getSeedAndElements = either fail pure . (=<<) decodeGoldenJson
+
+decodeGoldenJson :: FromJSON a => Aeson.Object -> Either String (Seed, Seq a)
+decodeGoldenJson = Aeson.parseEither $ \obj -> do
+  value <- obj .: "seed" >>= (.: "value")
+  gamma <- obj .: "seed" >>= (.: "gamma")
+  samples <- obj .: "samples"
+  pure (Seed value gamma, samples)
+
+compareWith :: MonadTest m => Show a => Eq a => Gen a -> Seed -> Seq a -> m ()
+compareWith gen seed samplesFromFile =
+  let
+    newSamples = genSamples seed gen
+  in
+    newSamples === samplesFromFile
