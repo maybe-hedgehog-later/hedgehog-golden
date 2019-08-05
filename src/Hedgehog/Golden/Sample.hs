c9db2d604b33aed421882cca3dd91b83cf5dc5bb
Felix Mulder <felix.mulder@gmail.com>
2019-08-05 16:24:35 +0200

    Support old version of hedgehog

--- src/Hedgehog/Golden/Sample.hs
+++ src/Hedgehog/Golden/Sample.hs
@@ -4,22 +4,29 @@ module Hedgehog.Golden.Sample
 
 import           Prelude
 
+import           Data.Functor.Identity (runIdentity)
 import           Data.Sequence (Seq)
 import           Hedgehog
 import qualified Hedgehog.Internal.Gen as Gen
 import qualified Hedgehog.Internal.Tree as Tree
 import qualified Hedgehog.Range as Range
+import           Control.Monad.Trans.Maybe (MaybeT(..))
 
 -- | Generate a fixed Seq of @a@ from the given generator
 genSamples :: Seed -> Gen a -> Seq a
 genSamples seed gen =
   let
     loop n = \case
-      Just tree -> Tree.treeValue tree
+      Just tree -> tree
       Nothing ->
         if n < 0 then
           error "Too many discards - abandoned generating samples"
         else
-          loop (n - 1) . Gen.evalGen 0 seed $ Gen.seq (Range.singleton 10) gen
+          loop (n - 1)
+            . fmap Tree.nodeValue
+            . runIdentity
+            . runMaybeT
+            . Tree.runTree
+            . Gen.runGenT 0 seed $ Gen.seq (Range.singleton 10) gen
   in
     loop (100 :: Int) Nothing
--- test/Main.hs
+++ test/Main.hs
@@ -3,11 +3,12 @@ module Main (main) where
 
 import           Prelude
 
+import           Control.Monad (when)
 import           Hedgehog (Property)
 import           Hedgehog (checkParallel, discover)
-import           Hedgehog.Main (defaultMain)
 import qualified Hedgehog.Golden.Aeson as Aeson
 import qualified Hedgehog.Gen as Gen
+import           System.Exit (exitFailure)
 
 prop_hexit :: Property
 prop_hexit = Aeson.goldenProperty Gen.hexit
@@ -19,4 +20,4 @@ tests :: IO Bool
 tests = checkParallel $$discover
 
 main :: IO ()
-main = defaultMain [ tests ]
+main = tests >>= flip when exitFailure
