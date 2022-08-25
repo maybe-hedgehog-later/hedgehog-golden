064a2685119cb777aa6e00a29c0ca0c94f8f343e
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2022-08-25 21:24:01 +0200

    Switch to nix flake with haskell.nix and stack build plan
    Co-authored-by: Paweł Szulc <paul.szulc@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  nixConfig.bash-prompt = "[nix-develop-hedgehog-golden:] ";
+  description = "hedgehog-golden";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachSystem ["x86_64-linux" ] (system:
+      let
+        deferPluginErrors = true;
+        overlays = [
+          haskellNix.overlay
+          (final: prev: {
+            # This overlay adds our project to pkgs
+            hedgehog-golden =
+              final.haskell-nix.project' {
+                src = ./.;
+                compiler-nix-name = "ghc923";
+                projectFileName = "stack.yaml";
+                modules = [{
+                  packages = { };
+                }];
+                shell.buildInputs = with pkgs; [
+                  cabal-install
+                  ghcid
+                  hlint
+                  nixpkgs-fmt
+                  stylish-haskell
+                ];
+              };
+          })
+        ];
+        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+        flake = pkgs.hedgehog-golden.flake { };
+      in
+      flake);
+}
